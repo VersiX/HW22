68ce65971c24e02301b8f2d79dd5a390cb995e3f
VersiX <yurkatj7@gmail.com>
2020-03-17 20:22:47 +0200

    fix

--- script/func.js
+++ script/func.js
@@ -1,35 +1,36 @@
 function func1() {
-    var name = prompt("Please enter your name:");
+    let name = prompt("Please enter your name:");
+    console.log("hahahaha " + name + " What a stupid name! lol");
     alert("Hello " + name);
-    console.log("lol" + name + " What a stupid name!");
+
 }
 
 function func2() {
     const currentYear = 2020;
-    var birthYear = prompt("Please enter your birth year:");
+    let birthYear = prompt("Please enter your birth year:");
     alert("Your age is " + (currentYear - birthYear));
 }
 
 function func3() {
-    var sqSide = prompt("Please enter square side lenght:");
+    let sqSide = prompt("Please enter square side lenght:");
     alert("Square perimeter is " + (sqSide * 4));
 }
 
 function func4() {
-    var circleRadius = prompt("Please enter circle radius:");
-    alert("Square perimeter is " + ((circleRadius ^ 2) * 3.14159265359));
+    let circleRadius = prompt("Please enter circle radius:");
+    alert("Circle lenght is " + ((circleRadius ^ 2) * Math.PI));
 }
 
 function func5() {
-    var distance = prompt("Please enter distance between cities:");
-    var time = prompt("Please enter how much time you have:");
+    let distance = prompt("Please enter distance between cities:");
+    let time = prompt("Please enter how much time you have:");
     alert("In order to drive " + distance + "km in "
         + time + " hours your speed must be " + (distance / time) + "km/h");
 }
 
 function func6() {
     const exchangeRate = 1.11;
-    var sum = prompt("Please enter money sum in euros:");
+    let sum = prompt("Please enter money sum in euros:");
     alert("your sum in USD " + (sum * exchangeRate));
 
 }
@@ -37,23 +38,23 @@ function func6() {
 function func7() {
     const fileSize = 820;
     const GBtoMB = 1024;
-    var flashSize = prompt("Please enter your flash size in GB:");
-    var leftSpace = (flashSize * GBtoMB) % fileSize;
+    let flashSize = prompt("Please enter your flash size in GB:");
+    let leftSpace = (flashSize * GBtoMB) % fileSize;
     alert("you can put " + (((flashSize * GBtoMB) - leftSpace) / fileSize) +
         " file(s) and have " + leftSpace + " MB free");
 
 }
 
 function func8() {
-    var money = prompt("Please enter how much money you can spend");
-    var price = prompt("Please enter how much money one chockolate bar costs");
-    var leftMoney = money % price;
+    let money = prompt("Please enter how much money you can spend");
+    let price = prompt("Please enter how much money one chockolate bar costs");
+    let leftMoney = money % price;
     alert("you can buy " + ((money - leftMoney) / price) +
         " chockolate bars and have " + leftMoney + " money left");
 }
 
 function func9() {
-    var n = prompt("Please enter your number");
+    let n = prompt("Please enter your number");
 
 
     alert((n % 10) * 100 + ((n - (n % 10)) % 100) + ((n - (n % 100)) % 1000) / 100);
@@ -62,7 +63,7 @@ function func9() {
 function func10() {
     const depositRate = 0.05;
     const mounths = 2;
-    var sum = prompt("Please enter how much money you want to put on deposit:")
+    let sum = prompt("Please enter how much money you want to put on deposit:")
     alert("After " + mounths + " you will get " + (sum * depositRate / mounths))
 
 }
